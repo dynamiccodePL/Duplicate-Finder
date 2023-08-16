66107057a5b3960fe7431ce24d4e2c37e1f9372e
Sebastian <65468884+dynamiccodePL@users.noreply.github.com>
2023-08-16 22:57:27 +0200

    Update example2.php

--- examples/example2.php
+++ examples/example2.php
@@ -1,1 +1,7 @@
+<?php
 
+$df = new DuplicateFinder("Lorem ipsum dolor Lorem sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip Lorem ex dolore ea commodo consequat. Duis aute irure dolor in reprehenderit ex in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
+
+$duplicates = $df->search_duplicates();
+
+echo $df->mark_duplicates();
