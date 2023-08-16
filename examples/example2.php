7599d7bde48faabdae9abaa1e9fd480009a6790a
Sebastian <65468884+dynamiccodePL@users.noreply.github.com>
2023-08-16 23:00:17 +0200

    Update example2.php

--- examples/example2.php
+++ examples/example2.php
@@ -1,5 +1,7 @@
 <?php
 
+# Highlighting duplicates in the text.
+
 $df = new DuplicateFinder("Lorem ipsum dolor Lorem sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip Lorem ex dolore ea commodo consequat. Duis aute irure dolor in reprehenderit ex in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
 
 $duplicates = $df->search_duplicates();
